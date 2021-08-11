9f5a66706659f33f512f11a5e5514d68e4727cfd
fjaram18 <41440657+fjaram18@users.noreply.github.com>
2021-08-11 13:46:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
-    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: light)");
 
     const theme = React.useMemo(
         () =>
