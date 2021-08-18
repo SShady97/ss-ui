67aad48a20efc4d12518040fef782a4828e29b6c
Jose Alvarez <joseez182@gmail.com>
2021-08-18 17:41:11 -0500

    .

--- src/components/Home/ProcessQueue/SavedQueues.jsx
+++ src/components/Home/ProcessQueue/SavedQueues.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:src/components/Home/ProcessQueue/SavedQueues.jsx
 import React, { useContext } from "react";
 import { TextField } from "@material-ui/core";
-=======
-import React from "react";
-import { TextField, makeStyles } from "@material-ui/core";
->>>>>>> 9f5a66706659f33f512f11a5e5514d68e4727cfd:src/components/Home/SavedProcesses.jsx
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import processQContext from '../../../context/processQ/processQContext';
@@ -15,19 +10,6 @@ const SavedQueues = ({ setSelected }) => {
 
     const  { savedQueues } = processesQContext;
 
-<<<<<<< HEAD:src/components/Home/ProcessQueue/SavedQueues.jsx
-=======
-const useStyles = makeStyles((theme) => ({
-    buscador: {
-        width: '100%',
-        backgroundColor: theme.palette.background.default,
-    }
-}));
-
-const SavedProcesses = () => {
-    const classes = useStyles();
-
->>>>>>> 9f5a66706659f33f512f11a5e5514d68e4727cfd:src/components/Home/SavedProcesses.jsx
     const handleChange = (event, newValue) => {
         setSelected(newValue);
     };
@@ -39,7 +21,6 @@ const SavedProcesses = () => {
             options={savedQueues}
             getOptionLabel={(option) => option.alias}
             onChange={handleChange}
-            className={classes.buscador}
             renderInput={(params) => (
                 <TextField
                     {...params}
